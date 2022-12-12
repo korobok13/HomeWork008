bdd6777efa14bc1ad26929c60f9e7048910f465a
korobov_o <korobok13@yandex.ru>
2022-12-12 20:32:47 +0300

    первая задача

--- HomeWork081/Program.cs
+++ HomeWork081/Program.cs
@@ -10,64 +10,59 @@
 // 9 5 3 2
 // 8 4 4 2
 
-var arr = FillMatrix(3, 4);
-PrintMatrix(arr);
-Console.WriteLine();
-var arr1 = SetNumbers(arr);
-Console.WriteLine(arr1);
 
-int[,] SetNumbers(int[,] arr)
+int[,] GetArray(int m, int n, int minValue, int maxValue)
 {
-    for (int i = 0; i > arr.GetLength(0); i++)
+    int[,] result = new int[m, n];
+
+    for (int i = 0; i < m; i++)
     {
-        int num = 0;
-        int max = 0;
-        for (int j = 0; j < arr.GetLength(1); j++)
+        for (int j = 0; j < n; j++)
         {
-
-            int k = 0;
-            while (k != arr.GetLength(1))
-            {
-                if (max < arr[i, j])
-                {
-                    max = arr[i, j];
-                    j++;
-                }
-                
-                arr[i, j] = arr[i, j - k];
-                k++;
-            }
-
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
         }
-    }
-    return arr;
+
+    }       
+    return result;
 }
 
-
-int[,] FillMatrix(int rows, int colums)
+void PrintArray(int[,] inArray)
 {
-    int[,] matrix = new int[rows, colums];
-    Random rnd = new Random();
-    for (int i = 0; i < matrix.GetLength(0); i++)
+    for (int i = 0; i < inArray.GetLength(0); i++)
     {
-        for (int j = 0; j < matrix.GetLength(1); j++)
+        for (int j = 0; j < inArray.GetLength(1); j++)
         {
-            matrix[i, j] = rnd.Next(0, 25);
-        }
-    }
-    return matrix;
-}
-
-
-void PrintMatrix(int[,] matrix)
-{
-    for (int i = 0; i < matrix.GetLength(0); i++)
-    {
-        for (int j = 0; j < matrix.GetLength(1); j++)
-        {
-            Console.Write($"{matrix[i, j]}, ");
+            Console.Write($"{inArray[i, j]}\t ");
         }
         Console.WriteLine();
     }
 }
+void SortRowsInArray(int[,] inArray)
+{
+    for (int k = 0; k < inArray.GetLength(0); k++)
+    {
+        for (int i = 0; i < inArray.GetLength(1); i++)
+        {
+        for (int j = i + 1; j < inArray.GetLength(1); j++)
+        {
+            if (inArray[k,i] < inArray[k,j])
+            {
+                int temp = inArray[k,i];
+                inArray[k,i] = inArray[k,j];
+                inArray[k,j] = temp;
+            }
+        }
+    }
+}
+}
 
+Console.Clear();
+Console.Write("Введите количество строк в массиве: ");
+int row = int.Parse(Console.ReadLine()!);
+Console.Write("Введите количество столбцов в массиве: ");
+int columns = int.Parse(Console.ReadLine()!);
+int[,] array = GetArray(row, columns, 0, 9);
+PrintArray(array);
+Console.WriteLine();
+SortRowsInArray(array);
+PrintArray(array);
